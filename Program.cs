96c8ab820f0914c2777b41eb3554e422cb3685b6
Anthony Warrington <Anthony.Warrington@gallagher.com>
2021-07-06 14:45:17 +1200

    Removed some references I didn't use

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
